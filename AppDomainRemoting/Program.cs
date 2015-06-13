745d3cc5338b32910e35f24fec1171aa24ca7bb4
Joe <wuzhou88@gmail.com>
2015-06-13 17:19:11 +1000

    Add AppDomainRemoting project

---
+++ AppDomainRemoting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainRemoting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AppDomain newDomain = AppDomain.CreateDomain("New Domain");
+
+
+            Foo foo = (Foo)newDomain.CreateInstanceAndUnwrap(
+                typeof(Foo).Assembly.FullName,
+                typeof(Foo).FullName);
+
+            Console.WriteLine(foo.SayHello());
+
+            AppDomain.Unload(newDomain);
+            Console.ReadLine();
+        }
+    }
+
+    public class Foo: MarshalByRefObject
+    {
+
+        public string SayHello()
+        {
+            return "hello from " + AppDomain.CurrentDomain.FriendlyName;
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+    }
+}
